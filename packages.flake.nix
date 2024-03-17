616106173e5bcee2ce15fb87caec10e33aa2a2bb
Maximilian Huber <gh@maxhbr.de>
2024-03-17 08:16:49 +0100

    update, just keep the flake one
    Signed-off-by: Maximilian Huber <gh@maxhbr.de>

--- packages.flake.nix
+++ packages.flake.nix
@@ -10,28 +10,19 @@ let
 in
 forAllSystems (system:
 let pkgs = nixpkgsFor."${system}"; in {
-  # build from this repo:
   dwl = (pkgs.dwl.overrideAttrs (prev: {
-      version = "git";
-      src = ./.;
-      enableXWayland = true;
-  }));
-  mydwl = self.packages.x86_64-linux.dwl.override { conf = ./myconfig.h; };
-
-  # build from git pulled in via flake:
-  dwl-git = (pkgs.dwl.overrideAttrs (prev: {
       version = "git";
       src = inputs.dwl;
       enableXWayland = true;
   }));
-  dwl-patched-git = self.packages.x86_64-linux.dwl-git.overrideAttrs (oldAttrs: rec {
+  dwl-patched = self.packages.x86_64-linux.dwl.overrideAttrs (oldAttrs: rec {
     patches = [
       # "${inputs.dwl-patches}/vanitygaps/vanitygaps.patch"
       "${inputs.dwl-patches}/centeredmaster/centeredmaster.patch"
       "${inputs.dwl-patches}/rotatetags/rotatetags.patch"
     ];
   });
-  mydwl-git = self.packages.x86_64-linux.dwl-patched-git.override { conf = ./myconfig.h; };
+  mydwl = self.packages.x86_64-linux.dwl-patched.override { conf = ./myconfig.h; };
 
   mysomebar =  (pkgs.somebar.overrideAttrs (prev: {
     version = "git";
